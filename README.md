9981c1732991715c04f578820449709933feb18e
aooiuu <aooiu@qq.com>
2023-12-12 18:29:38 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# eruda-track
+
+[![npm version][npm-version-src]][npm-version-href]
+[![bundle][bundle-src]][bundle-href]
+[![JSDocs][jsdocs-src]][jsdocs-href]
+
+Eruda 埋点插件
+
+## Usage
+
+```sh
+npm i eruda-track
+```
+
+```javascript
+import erudaTrack from 'eruda-track';
+
+eruda.add(erudaTrack);
+```
+
+<!-- Badges -->
+
+[npm-version-src]: https://img.shields.io/npm/v/eruda-track?style=flat&colorA=18181B&colorB=F0DB4F
+[npm-version-href]: https://npmjs.com/package/eruda-track
+[bundle-src]: https://img.shields.io/bundlephobia/minzip/eruda-track?style=flat&colorA=18181B&colorB=F0DB4F
+[bundle-href]: https://bundlephobia.com/result?p=eruda-track
+[jsdocs-src]: https://img.shields.io/badge/jsDocs.io-reference-18181B?style=flat&colorA=18181B&colorB=F0DB4F
+[jsdocs-href]: https://www.jsdocs.io/package/eruda-track
---
+++ src/index.ts
@@ -0,0 +1,119 @@
+import LS from 'lz-string'
+import LunaDataGrid from 'luna-data-grid'
+import throttle from 'licia/throttle'
+import ResizeSensor from 'licia/ResizeSensor'
+import dateFormat from 'licia/dateFormat'
+
+const HOST = 'api.growingio.com'
+
+export default function (eruda: any) {
+  class Plugin extends eruda.Tool {
+    constructor() {
+      super()
+      this.name = 'gio'
+    }
+
+    init($el: any, container: any) {
+      super.init($el, container)
+      this._container = container
+      this._initTpl()
+
+      this._requestDataGrid = new LunaDataGrid(this._$requests.get(0), {
+        columns: [
+          {
+            id: 't',
+            title: 'Type',
+          },
+          {
+            id: 'n',
+            title: 'EventId',
+          },
+          {
+            id: '_tm',
+            title: 'Time',
+            sortable: true,
+          },
+        ],
+      })
+      this._resizeSensor = new ResizeSensor($el.get(0))
+      this._bindEvent()
+    }
+
+    show() {
+      super.show()
+      this._updateDataGridHeight()
+    }
+
+    hide() {
+      super.hide()
+    }
+
+    destroy() {
+      super.destroy()
+      this._resizeSensor.destroy()
+    }
+
+    _updateDataGridHeight() {
+      const height = this._$el.offset().height - this._$control.offset().height
+      this._requestDataGrid.setOption({
+        minHeight: height,
+        maxHeight: height,
+      })
+    }
+
+    _initTpl() {
+      const $el = this._$el
+      $el.html(
+        `<div class="eruda-network">
+          <div class="eruda-control">
+          </div>
+          <div class="eruda-requests"></div>
+        </div>
+        <div class="eruda-detail"></div>`,
+      )
+      this._$requests = $el.find('.eruda-requests')
+      this._$control = $el.find('.eruda-control')
+    }
+
+    _bindEvent() {
+      this._resizeSensor.addListener(
+        throttle(() => this._updateDataGridHeight(), 15),
+      )
+
+      const sendBeacon = window.navigator.sendBeacon
+      this._sendBeacon = sendBeacon
+      const onBeacon = this._onBeacon.bind(this)
+      window.navigator.sendBeacon = function (...arg) {
+        onBeacon(arg[0] as string, arg[1] as Uint8Array)
+        return sendBeacon.apply(this, arg)
+      }
+    }
+
+    _onBeacon(url: string, data: Uint8Array) {
+      if (!url.includes(HOST))
+        return
+      try {
+        this._addRow(JSON.parse(LS.decompressFromUint8Array(data)))
+      }
+      catch (error) {
+        console.warn(error)
+      }
+    }
+
+    _addRow(data: any) {
+      if (Array.isArray(data)) {
+        data.forEach(row => this._addRow(row))
+        return
+      }
+
+      if (data?.t !== 'cstm')
+        return
+      this._requestDataGrid.append({
+        ...data,
+        _tm: dateFormat(new Date(data.tm), 'isoTime'),
+      }, { selectable: true })
+    }
+  }
+
+  return new Plugin()
+}
